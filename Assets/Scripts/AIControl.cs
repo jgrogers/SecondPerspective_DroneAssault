afd92dca04080501f66fccc58b62257a8852fa09
John Rogers <john.g.rogers59.civ@mail.mil>
2023-05-21 18:13:10 -0400

    AI waypoint controller created, need state machine

--- Assets/Scripts/AIControl.cs
+++ Assets/Scripts/AIControl.cs
@@ -13,6 +13,9 @@ public class AIControl : MonoBehaviour
     [SerializeField] private GameObject cannonRotation;
     [SerializeField] private Transform FiringPoint;
     [SerializeField] private GameObject Shell;
+    [SerializeField] private WheelCollider[] leftTracks;
+    [SerializeField] private WheelCollider[] rightTracks;
+    [SerializeField] private float motorTorque = 150.0f;
     [SerializeField] private float turretMoveRate = 4.0f;
     [SerializeField] private float azimuth = 0.0f;
     [SerializeField] private float elevation = -15.0f;
@@ -25,6 +28,7 @@ public class AIControl : MonoBehaviour
     [SerializeField] private float learnedDistanceScale = 1.0f;
     [SerializeField] private float ErrorRadius = 15.0f;
     [SerializeField] private Vector2 currentError;
+    [SerializeField] private Transform goalPoint;
     // Start is called before the first frame update
     void Start()
     {
@@ -42,59 +46,119 @@ public class AIControl : MonoBehaviour
     void Update()
     {
         if (target) {
-            if (!GameManager.Instance.CheckObjectExists(target)) {
-                Debug.Log("Target gone");
-                target = null;
+            VerifyTargetStillExists();
+        }
+        if (!target) {
+            FindNewTarget();
+        }
+        if (target) {
+            ComputeFiringSolution();
+        } else {
+            //Continue on primary mission: get to goalPoint
+        }
+    }
+    void FixedUpdate() {
+        //Orient turret toward target if visible
+        // Fire if we are oriented correctly
+        if (targetInRange && targetVisible && hostile) {
+            AimAndFire();
+        } else {
+            //Continue on primary mission: get to goalPoint
+            if(TurnToGoal()) {
+                MoveToGoal();
+            }
+        }
+    }
+    private bool TurnToGoal() {
+        //Does a turn-in-place behavior if we are not facing pretty close to the goal, otherwise return true;
+        Vector3 direction = goalPoint.transform.position - transform.position;
+        Vector3 localDirection = transform.InverseTransformDirection(direction);
+        float yaw_error = Mathf.Atan2(localDirection.x, localDirection.z);
+        if (Mathf.Abs(yaw_error) < Mathf.Deg2Rad * 30.0f) return true;
+        else {
+            float leftTorque = 0.0f;
+            float rightTorque = 0.0f;
+            if (WrapToPi(yaw_error) > 0) {
+                leftTorque = motorTorque;
+                rightTorque = -motorTorque;
+            } else {
+                leftTorque = -motorTorque;
+                rightTorque = motorTorque;
+            }
+            foreach(WheelCollider wheel in leftTracks) {
+                wheel.motorTorque = leftTorque;
+            }
+            foreach(WheelCollider wheel in rightTracks) {
+                wheel.motorTorque = rightTorque;
+            }
+        }
+        return false;
+    }
+    private bool MoveToGoal() {
+        Vector3 direction = goalPoint.transform.position - transform.position;
+        Vector3 localDirection = transform.InverseTransformDirection(direction);
+        float yaw_error = Mathf.Atan2(localDirection.x, localDirection.z);
+        
+        float leftTorque = motorTorque;
+        float rightTorque = motorTorque;
+        if (yaw_error < 0) leftTorque = leftTorque * Mathf.Max(1.0f, 0.3f/ yaw_error);
+        else if (yaw_error > 0) rightTorque = rightTorque * Mathf.Max(1.0f, 0.3f/ yaw_error);
+        foreach(WheelCollider wheel in leftTracks) {
+            wheel.motorTorque = leftTorque;
+        }
+        foreach(WheelCollider wheel in rightTracks) {
+            wheel.motorTorque = rightTorque;
+        }
+        return direction.magnitude < 3.0f;
+    }
+    private void VerifyTargetStillExists() {
+        if (!GameManager.Instance.CheckObjectExists(target)) {
+            target = null;
+        }
+    }
+    private void FindNewTarget() {
+        //Try to find targets
+        foreach (GameObject allyTeamMember in GameManager.Instance.PlayerTeam) {
+            Vector3 direction = allyTeamMember.transform.position - transform.position;
+            Debug.DrawRay(sensorPoint.position, direction);
+            if (Physics.Raycast(sensorPoint.position, direction, out RaycastHit hit, detectionRange)) {
+                if (hit.transform == allyTeamMember.transform) {
+                    target = allyTeamMember;
+                    Debug.Log("Found target " + target);
+                    break;
+                } else {
+                    Debug.Log("Trying to get target but hit " + hit.transform);
+                }
             }
         }
         if (target == null) {
-            //Try to find targets
-            foreach (GameObject allyTeamMember in GameManager.Instance.PlayerTeam) {
-                Vector3 direction = allyTeamMember.transform.position - transform.position;
-                Debug.DrawRay(sensorPoint.position, direction);
-                if (Physics.Raycast(sensorPoint.position, direction, out RaycastHit hit, detectionRange)) {
-                    if (hit.transform == allyTeamMember.transform) {
-                        target = allyTeamMember;
-                        Debug.Log("Found target " + target);
-                        break;
-                    } else {
-                        Debug.Log("Trying to get target but hit " + hit.transform);
-                    }
-                }
-            }
-            if (target == null) {
+            targetVisible = false;
+        }
+    }
+    private void ComputeFiringSolution() {
+        // Check raycast to see if target is still visible
+        Vector3 direction = target.transform.position - transform.position;
+        Vector3 localDirection = transform.InverseTransformDirection(direction);
+        if (Physics.Raycast(sensorPoint.position, direction, out RaycastHit hit, Mathf.Infinity)) {
+            if (hit.transform == target.transform) {
+                targetVisible = true;
+            } else {
                 targetVisible = false;
             }
         }
-        if (target) {
-            // Check raycast to see if target is still visible
-            Vector3 direction = target.transform.position - transform.position;
-            Vector3 localDirection = transform.InverseTransformDirection(direction);
-            if (Physics.Raycast(sensorPoint.position, direction, out RaycastHit hit, Mathf.Infinity)) {
-                if (hit.transform == target.transform) {
-                    targetVisible = true;
-                } else {
-                    targetVisible = false;
-                }
-            }
-            if (targetVisible) {
-                
-                float dx = localDirection.x + currentError.x;
-                float dy = localDirection.y;
-                float dz = localDirection.z + currentError.y;
-                azimuthGoal = Mathf.Rad2Deg * Mathf.Atan2(dx, dz);
-                float horiz_dist = learnedDistanceScale * Mathf.Sqrt(dx*dx + dz*dz);
-//                elevationGoal = Mathf.Rad2Deg * (Mathf.Atan2(dy, horiz_dist) - elevationAimGain * horiz_dist);
-                float asin_value = horiz_dist * 9.81f / (30.0f * 30.0f);
-                if (asin_value >= 1.0f || asin_value < 0) targetInRange = false;
-                else targetInRange = true;
-                if(targetInRange) {
-                    elevationGoal = Mathf.Rad2Deg * (0.5f * Mathf.Asin(horiz_dist * 9.81f / (30.0f * 30.0f)));
-                } else {
-                    elevationGoal = 45.0f;
-                }
+        if (targetVisible) {
+            float dx = localDirection.x + currentError.x;
+            float dy = localDirection.y;
+            float dz = localDirection.z + currentError.y;
+            azimuthGoal = Mathf.Rad2Deg * Mathf.Atan2(dx, dz);
+            float horiz_dist = learnedDistanceScale * Mathf.Sqrt(dx*dx + dz*dz);
+            float asin_value = horiz_dist * 9.81f / (30.0f * 30.0f);
+            if (asin_value >= 1.0f || asin_value < 0) targetInRange = false;
+            else targetInRange = true;
+            if(targetInRange) {
+                elevationGoal = Mathf.Rad2Deg * (0.5f * Mathf.Asin(horiz_dist * 9.81f / (30.0f * 30.0f)));
             } else {
-                Debug.Log("I'd move towards target");
+                elevationGoal = 45.0f;
             }
         }
     }
@@ -107,9 +171,7 @@ public class AIControl : MonoBehaviour
         }
         return ang_in;
     }
-    void FixedUpdate() {
-        //Orient turret toward target if visible
-        // Fire if we are oriented correctly
+    private void AimAndFire() {
         bool onTarget = false;
         float azimuthError = WrapToPi(azimuthGoal - azimuth);
         float dAzimuth =  azimuthError * Time.deltaTime * turretMoveRate;
@@ -131,11 +193,11 @@ public class AIControl : MonoBehaviour
         cannonElevation.transform.localRotation = Quaternion.Euler(0.0f, 0.0f, elevation);
         if (targetInRange && targetVisible && onTarget && hostile) {
             if (fireControl.Fire()) {
-                Debug.Log("Firing shell!");
                 GameObject shell = Instantiate(Shell, FiringPoint.position, FiringPoint.rotation);
                 shell.GetComponent<Shell>().SetSafeMask(gameObject.layer);
                 SampleDistanceError();
             } 
         }
+
     }
 }
