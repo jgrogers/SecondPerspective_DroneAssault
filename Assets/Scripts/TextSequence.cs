9ef71c222f2b97ad4d080f184d621e7517fad524
John Rogers <john.g.rogers59.civ@mail.mil>
2023-05-27 17:12:43 -0400

    Adding tutorial scene

---
+++ Assets/Scripts/TextSequence.cs
@@ -0,0 +1,82 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class TextSequence : MonoBehaviour
+{
+    [System.Serializable]
+    public struct TextEvent {
+        public string text;
+        public float Duration;
+        public float pauseAfter;
+        public int whichBox;
+        public string responseEventToSee;
+    };
+    public Dictionary<string, bool> eventLabelSeen;
+    [SerializeField] private List<TextEvent> events;
+    [SerializeField] private List<TextMeshProUGUI> textBox;
+    [SerializeField] private List<Image> textBackgrounds;
+    [SerializeField] private PlayerControl player;
+
+    // Start is called before the first frame update
+    // Update is called once per frame
+    void Start()
+    {
+        eventLabelSeen = new Dictionary<string, bool>();
+        foreach(Image image in textBackgrounds) {
+            image.enabled = false;
+        }
+        foreach(TextMeshProUGUI text in textBox) {
+            text.text = "";
+        }
+        player.onFireCannon += onFireCannon;
+        player.onForwardBack += onForwardBack;
+        player.onTurn += onTurn;
+        player.onTurretTurn += onTurretTurn;
+        player.onBarrelAim += onBarrelAim;
+        StartCoroutine(TextSequencer());
+    }
+    public void onFireCannon(object origin, EventArgs e) {
+        eventLabelSeen["cannon_fire"] = true;
+    }
+    public void onForwardBack(object origin, EventArgs e) {
+        eventLabelSeen["forward_back"] = true;
+    }
+    public void onTurn(object origin, EventArgs e) {
+        eventLabelSeen["turn"] = true;
+    }
+    public void onTurretTurn(object origin, EventArgs e) {
+        eventLabelSeen["turret_turn"] = true;
+    }
+    public void onBarrelAim(object origin, EventArgs e) {
+        eventLabelSeen["barrel_aim"] = true;
+    }
+    private IEnumerator TextSequencer() {
+        foreach (TextEvent e in events) {
+            textBox[e.whichBox].text = e.text;
+            textBackgrounds[e.whichBox].enabled = true;
+            float startTime = Time.time;
+            if (e.responseEventToSee != "") {
+                eventLabelSeen[e.responseEventToSee] = false;
+            }
+            while (Time.time < startTime + e.Duration) {
+                yield return null;
+            }
+            if (e.responseEventToSee != "") {
+                while (!eventLabelSeen[e.responseEventToSee]) {
+                    yield return null;
+                }
+            }
+            textBox[e.whichBox].text = "";
+            textBackgrounds[e.whichBox].enabled = false;
+            startTime = Time.time;
+            while (Time.time < startTime + e.pauseAfter) {
+                yield return null;
+            }
+        }
+    }
+}
